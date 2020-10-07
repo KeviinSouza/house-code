29e8d2b1df8b5a5f5920bdf6491b7c73eebccc30
Kevin Souza <kevinsou113@gmail.com>
2020-10-07 17:02:35 -0300

    Livro Bean
      Atualização de anotação Manage Bean para @model.
    Anotação @Transactional adicionada ao método salvar para inversão de controle ao JTA. OBS: necessario JTA 1.2 como dependencia para uso dessa anotação.
    Adicionado faces-redrect ao método salvar para list.xhtml.

--- src/main/java/br/com/housecode/beans/LivroBean.java
+++ src/main/java/br/com/housecode/beans/LivroBean.java
@@ -1,26 +1,47 @@
 package br.com.housecode.beans;
 
-import javax.enterprise.context.RequestScoped;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.enterprise.inject.Model;
 import javax.inject.Inject;
-import javax.inject.Named;
+import javax.transaction.Transactional;
+import br.com.housecode.dao.LivroDAO;
+import br.com.housecode.models.Autor;
 import br.com.housecode.models.Livro;
 
-
-@Named
-@RequestScoped
+@Model
 public class LivroBean {
-	
+
 	@Inject
 	private Livro livro;
+	@Inject
+	private LivroDAO livrodao;
 	
-	public void Salvar() {
-		System.out.println("Livro " + livro +  "Cadastrado Com Sucesso!!");
+	private List<Integer> autoresId = new ArrayList<>();
+	
+	@Transactional
+	public String Salvar() {
+		for (Integer autoresId : autoresId) {
+			livro.getAutores().add(new Autor(autoresId));
+		}
+		livrodao.salvar(livro);
+		
+		return "/livros/list?faces-redirect=true";
 	}
-	
+
+	public List<Integer> getAutoresId() {
+		return autoresId;
+	}
+
+	public void setAutoresId(List<Integer> autoresId) {
+		this.autoresId = autoresId;
+	}
+
 	public Livro getLivro() {
 		return livro;
 	}
-	
+
 	public void setLivro(Livro livro) {
 		this.livro = livro;
 	}
