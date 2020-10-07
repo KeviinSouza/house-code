3733fc18218fe68a9e29876e4329d6338ebcd4d9
Kevin Souza <kevinsou113@gmail.com>
2020-10-07 17:07:12 -0300

    Livro.java
      class livro Atualizada.
      Realacionamento de ManyToMany(MTM) criado para array.list de autores.

--- src/main/java/br/com/housecode/models/Livro.java
+++ src/main/java/br/com/housecode/models/Livro.java
@@ -1,17 +1,21 @@
 package br.com.housecode.models;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Lob;
+import javax.persistence.ManyToMany;
 
 @Entity
 public class Livro {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String titulo;
 	
 	@Lob
@@ -19,11 +23,12 @@ public class Livro {
 	private String paginas;
 	private BigDecimal preco;
 	
+	@ManyToMany
+	private List<Autor> autores = new ArrayList<>();
+
 	
-	@Override
-	public String toString() {
-		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + ", preco=" + preco
-				+ "]";
+	public List<Autor> getAutores() {
+		return autores;
 	}
 	
 	public String getTitulo() {
@@ -49,6 +54,13 @@ public class Livro {
 	}
 	public void setPreco(BigDecimal preco) {
 		this.preco = preco;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "Livro [titulo=" + titulo + ", descricao=" + descricao + ", paginas=" + paginas + ", preco=" + preco
+				+ "]";
 	}
 
 }
