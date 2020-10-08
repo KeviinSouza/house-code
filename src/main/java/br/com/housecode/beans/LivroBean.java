3ac172b0b2c474f3da605b235bd46575e07578bf
Kevin Souza <kevinsou113@gmail.com>
2020-10-08 15:12:16 -0300

    Livro Bean
      Correção de inject de Livro, sendo transformado em atributo da class.
      Adicionado inject de autorDAO, para uso em setAutoresID utilizando método próprio listar().

--- src/main/java/br/com/housecode/beans/LivroBean.java
+++ src/main/java/br/com/housecode/beans/LivroBean.java
@@ -1,12 +1,14 @@
 package br.com.housecode.beans;
 
-import java.util.ArrayList;
 import java.util.List;
+
 import javax.enterprise.inject.Model;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
+
+import br.com.housecode.dao.AutorDAO;
 import br.com.housecode.dao.LivroDAO;
 import br.com.housecode.models.Autor;
 import br.com.housecode.models.Livro;
@@ -14,33 +16,28 @@ import br.com.housecode.models.Livro;
 @Model
 public class LivroBean {
 
-	@Inject
-	private Livro livro;
+	
+	private Livro livro = new Livro();
+	
 	@Inject
 	private LivroDAO livrodao;
 	@Inject
+	private AutorDAO autordao;
+	@Inject
 	private FacesContext fc;
-	
-	private List<Integer> autoresId = new ArrayList<>();
 	
 	@Transactional
 	public String Salvar() {
-		for (Integer autoresId : autoresId) {
-			livro.getAutores().add(new Autor(autoresId));
-		}
+	
 		fc.getExternalContext().getFlash().setKeepMessages(true);
 		fc.addMessage(null, new FacesMessage("Cadastro Realizado Com Sucesso!!"));
 		livrodao.salvar(livro);
 		
 		return "/livros/list?faces-redirect=true";
 	}
-
-	public List<Integer> getAutoresId() {
-		return autoresId;
-	}
-
-	public void setAutoresId(List<Integer> autoresId) {
-		this.autoresId = autoresId;
+	
+	public List<Autor> getAutores(){
+		return autordao.listar();
 	}
 
 	public Livro getLivro() {
